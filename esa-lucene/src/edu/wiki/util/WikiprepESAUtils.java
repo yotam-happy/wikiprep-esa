2446b479a8e5ab4cc5a31e18c3e3358dc4fe6a58
yotam-happy <yotamrockon2@yahoo.com>
2015-12-17 13:54:24 +0200

    added window of words service method the WikiprepESAUtils

--- esa-lucene/src/edu/wiki/util/WikiprepESAUtils.java
+++ esa-lucene/src/edu/wiki/util/WikiprepESAUtils.java
@@ -1,7 +1,15 @@
 package edu.wiki.util;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.lucene.analysis.CustomTokenizer;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
 
 public class WikiprepESAUtils {
 	/**
@@ -25,6 +33,43 @@ public class WikiprepESAUtils {
 			j = doc.indexOf('\n', i);
 		}
 		return contexts;
+	}
+	
+	public static Set<String> getWindowOfWordsContexts(String doc, int sz, int overlapSz) {
+		return getWindowOfWordsContexts(Arrays.asList(doc), sz, overlapSz);
+	}
+	public static Set<String> getWindowOfWordsContexts(Collection<String> largerContexts, int sz, int overlapSz) {
+		Set<String> result = new HashSet<String>();
+
+		largerContexts.forEach((context) -> {
+			try {
+				CustomTokenizer wordTokenizer = new CustomTokenizer(new StringReader(context));
+	
+				StringBuffer nextWindow = new StringBuffer();
+				StringBuffer window = new StringBuffer();
+				int count = 0;
+				while (wordTokenizer.incrementToken()) {
+					window.append(" ").append(wordTokenizer.getAttribute(TermAttribute.class).term());
+					if (sz - count >= overlapSz){
+						nextWindow.append(" ").append(wordTokenizer.getAttribute(TermAttribute.class).term());
+					}
+					count++;
+					if (count >= sz) {
+						result.add(window.toString());
+						window = nextWindow;
+						nextWindow = new StringBuffer();
+						count = overlapSz;
+					}
+				}
+				if (count > 0) {
+					result.add(window.toString());
+				}
+				wordTokenizer.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		});
+		return result;
 	}
 
 }
